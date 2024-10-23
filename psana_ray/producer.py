d54fa2bebbf3ad4f146b645114d892a630788635
Cong Wang <cwang31@stanford.edu>
2024-10-22 22:55:54 -0700

    Incorporated mask.

--- psana_ray/producer.py
+++ psana_ray/producer.py
@@ -18,6 +18,7 @@ def parse_arguments():
     parser.add_argument("--detector_name", type=str, required=True, help="Detector name")
     parser.add_argument("--calib", type=bool, default=True, help="Use calib mode")
     parser.add_argument("--uses_bad_pixel_mask", type=bool, default=True, help="Use bad pixel mask")
+    parser.add_argument("--manual_mask_path", type=str, default=None, help="Path to a manual mask in npy")
     parser.add_argument("--ray_address", type=str, default="auto", help="Address of the Ray cluster")
     parser.add_argument("--ray_namespace", type=str, default="default", help="Ray namespace to use for both queues")
     parser.add_argument("--queue_name", type=str, default='my', help="Queue name")
@@ -64,24 +65,27 @@ def signal_handler(sig, frame):
     ray.shutdown()
     exit(0)
 
-def produce_data(psana_wrapper, psana_mode, uses_bad_pixel_mask, queue, rank, size):
+def produce_data(psana_wrapper, psana_mode, queue, rank, size, uses_bad_pixel_mask=False, manual_mask_path=None):
     comm = MPI.COMM_WORLD
 
     bad_pixel_mask = psana_wrapper.create_bad_pixel_mask() if uses_bad_pixel_mask else None
+    manual_pixel_mask = np.load(manual_mask_path) if manual_mask_path is not None else None
 
     # Delay for exponential backoff
     base_delay_in_sec = 0.1
     max_delay_in_sec  = 2.0
 
-    for idx, data in enumerate(psana_wrapper.iter_events(mode=psana_mode)):
+    for idx, (data, photon_energy) in enumerate(psana_wrapper.iter_events(mode=psana_mode)):
         if bad_pixel_mask is not None:
             data = np.where(bad_pixel_mask, data, 0)
+        if manual_pixel_mask is not None:
+            data = np.where(manual_pixel_mask, data, 0)
         retries = 0
         while True:
             try:
-                success = ray.get(queue.put.remote([rank, idx, data]))
+                success = ray.get(queue.put.remote([rank, idx, data, photon_energy]))
                 if success:
-                    print(f"Rank {rank} produced: idx={idx} | shape={data.shape}")
+                    print(f"Rank {rank} produced: idx={idx} | shape={data.shape} | photon_energy={photon_energy}")
                     break  # Break the while loop and move to the next event
                 else:
                     print(f"Rank {rank}: Queue is full, waiting...")
@@ -136,7 +140,7 @@ def main():
     )['calib' if args.calib else 'image']
 
     try:
-        produce_data(psana_wrapper, psana_mode, args.uses_bad_pixel_mask, queue, rank, size)
+        produce_data(psana_wrapper, psana_mode, queue, rank, size, args.uses_bad_pixel_mask, args.manual_mask_path)
     except Exception as e:
         print(f"Rank {rank}: Unhandled exception in main: {e}")
     finally:
